230fc38b7da2b91edbcfd9226af37b2baa04bd76
Jeff Peiffer <jpeiffer@gmail.com>
2021-01-02 12:31:25 -0500

    Removed flutter build aot

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## [1.0.2] - Janurary 2nd, 2021
+
+* Removed `flutter build aot` as that's been formally removed from the CLI.
+
+
 ## [1.0.1] - June 8th, 2020
 
 * Fixed copy-and-paste errors
--- flutter-pre-commit.sh
+++ flutter-pre-commit.sh
@@ -5,9 +5,6 @@ flutter clean
 flutter packages upgrade
 flutter analyze
 flutter test
-if test -f "lib/main.dart"; then
-  flutter build aot  
-fi
 dartfmt -w --set-exit-if-changed lib test
 
 if test -f "example"; then
@@ -15,6 +12,5 @@ if test -f "example"; then
   flutter clean
   flutter packages upgrade
   flutter analyze
-  flutter build aot
   dartfmt -w --set-exit-if-changed lib 
 fi
